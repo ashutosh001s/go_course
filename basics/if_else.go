f752246410f62de373a793be6a3a66708cf5ebee
Ashutosh singh <66360078+ashutosh001s@users.noreply.github.com>
2025-04-07 18:48:18 +0530

    if else statement in go

---
+++ basics/if_else.go
@@ -0,0 +1,63 @@
+package basics
+
+import "fmt"
+
+func main() {
+
+	//age := 25
+
+	// if age >= 18 {
+	// 	fmt.Println("You are an adult")
+	// } else{
+
+	// 	fmt.Println("You are an kid")
+	// }
+	// temp := 39
+	// if temp >= 32 {
+	// 	fmt.Println("its hot outside")
+	// } else {
+	// 	fmt.Println("its cool outside")
+	// }
+
+	// score := 85
+	// if score >= 90 {
+	// 	fmt.Println("Grade A")
+	// } else if score >= 80 {
+	// 	fmt.Println("Grade B")
+	// } else if score >= 70 {
+	// 	fmt.Println("Grade C")
+	// } else {
+	// 	fmt.Println("Grade D")
+	// }
+
+	// if score >= 90 {
+	// 	fmt.Println("Grade A")
+	// }
+	// if score >= 80 {
+	// 	fmt.Println("Grade B")
+	// }
+	// if score >= 70 {
+	// 	fmt.Println("Grade C")
+	// } else {
+	// 	fmt.Println("Grade D")
+	// }
+
+	// for i := range 20 {
+	// 	if i == 0 {
+	// 		continue
+	// 	}
+	// 	if i%2 == 0 {
+	// 		if i%3 == 0 {
+	// 			fmt.Println(i, "is divisible by both 2 and 3")
+	// 		} else {
+	// 			fmt.Println(i, "is divisible by 2 but not 3")
+	// 		}
+	// 	} else {
+	// 		fmt.Println(i, "is not divisible 2")
+	// 	}
+	// }
+
+	if 10%2 == 0 || 5%2 == 0 {
+		fmt.Println("either 10 or 5 are even")
+	}
+}
